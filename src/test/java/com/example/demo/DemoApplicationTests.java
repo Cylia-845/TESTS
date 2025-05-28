22d6197b01ebe49bbd3b27eadb05303f47fabdb7
Cylia <zerrouki.cylia@gmail.com>
2025-05-28 18:05:38 +0200

    Update DemoApplicationTests.java

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -5,6 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,7 +17,26 @@ class DemoApplicationTests {
 
     @Test
     void homeEndpointShouldReturnHelloJenkins() {
-        String response = this.restTemplate.getForObject("/", String.class);
+        String response = this.restTemplate.getForObject("/api/", String.class);
         assertThat(response).isEqualTo("Hello Jenkins!");
+    }
+
+    @Test
+    void greetEndpointShouldReturnGreetingWithName() {
+        String response = this.restTemplate.getForObject("/api/greet?name=Cylia", String.class);
+        assertThat(response).isEqualTo("Hello, Cylia!");
+    }
+
+    @Test
+    void greetEndpointShouldReturnDefaultGreeting() {
+        String response = this.restTemplate.getForObject("/api/greet", String.class);
+        assertThat(response).isEqualTo("Hello, World!");
+    }
+
+    @Test
+    void addEndpointShouldReturnSum() {
+        ResponseEntity<Integer> response = this.restTemplate.postForEntity("/api/add?a=3&b=4", null, Integer.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isEqualTo(7);
     }
 }
